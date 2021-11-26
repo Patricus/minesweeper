93d1ccce7ff657253153c32a4c7d09b165c3c00d
patrick <patrick.j.mcpherson@gmail.com>
2021-11-26 07:20:41 -0800

    init search

--- board.rb
+++ board.rb
@@ -15,13 +15,60 @@ class Board
           line += 'F '
         elsif tile.revealed == false
           line += '* '
-          # TODO: add interior and fringe squares
+        else
+          if tile.fringe < 1
+            line += '_ '
+          else
+            line += "#{tile.fringe.to_s} "
+          end
         end
       end
       puts line
     end
   end
+
+  def [](position)
+    @grid[position.first][position.last]
+  end
+
+  def look_up(position)
+    pos_up = position.first - 1, position.last
+    return self[position].fringe += 1 if self[pos_up].bomb == true
+    self[pos_up].reveal
+    look_up(pos_up)
+  end
+
+  def look_down(position)
+    pos_down = position.first + 1, position.last
+    return self[position].fringe += 1 if self[pos_down].bomb == true
+    self[pos_down].reveal
+    look_down(pos_down)
+  end
+
+  def look_left(position)
+    pos_left = position.first, position.last - 1
+    return self[position].fringe += 1 if self[pos_left].bomb == true
+    self[pos_left].reveal
+    look_left(pos_left)
+  end
+
+  def look_right(position)
+    pos_right = position.first, position.last + 1
+    return self[position].fringe += 1 if self[pos_right].bomb == true
+    self[pos_right].reveal
+    look_right(pos_right)
+  end
+
+  def search_position(position)
+    self[position].reveal
+    look_up(position)
+    look_down(position)
+    look_left(position)
+    look_right(position)
+  end
 end
 
 board = Board.new
+board.render
+board.search_position([3, 3])
 board.render
--- tile.rb
+++ tile.rb
@@ -1,10 +1,12 @@
 class Tile
   attr_reader :bomb, :flagged, :revealed
+  attr_accessor :fringe
 
   def initialize(bomb)
     @bomb = bomb
     @flagged = false
     @revealed = false
+    @fringe = 0
   end
 
   def toggle_flag
